4e76af10f3eeb59180a5967383606dab8049074c
D-coder <chuksjoshuaa@gmail.com>
2022-10-03 12:16:33 +0100

    Updated Changes to Shazam Api

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "a9af11c38fmshc761ffe28560cf4p184146jsn1203d639b760",
-    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
-  },
-};
-
-fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
-
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: () => {
+      headers.set(
+        "X-RapidAPI-Key",
+        import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
+      );
+
+      return headers;
+    },
+  }),
 });
