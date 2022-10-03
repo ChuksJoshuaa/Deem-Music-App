b087a18902f969fd721fb23d1956b69d38b99e07
D-coder <chuksjoshuaa@gmail.com>
2022-10-03 13:22:51 +0100

    SongCard Updated

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,8 +5,16 @@ import { useDispatch } from "react-redux"
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, i }) => {
-  const activeSong = 'Test'
+const SongCard = ({ song, i, activeSong, isPlaying }) => {
+
+
+  const handlePauseClick = () => {
+    
+  }
+
+  const handlePlayClick = () => {
+
+  }
 
   return (
     <>
@@ -18,7 +26,13 @@ const SongCard = ({ song, i }) => {
       bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ?
               'flex bg-black bg-opacity-70' : 'hidden'}`}
           >
-           <PlayPause />
+            <PlayPause
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              song={song} 
+              handlePause={handlePauseClick}
+              handlePlay={handlePlayClick}
+            />
           </div>
           <img alt="song_img" src={song.images?.coverart}  />
         </div>
