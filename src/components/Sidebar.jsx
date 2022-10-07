c73b5dc7e3cc504679bbd77866a1bec911dbe993
D-coder <chuksjoshuaa@gmail.com>
2022-10-07 16:50:22 +0100

    Chnages made to Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { NavLink } from "react-router-dom"
 import { RiCloseLine } from "react-icons/ri"
+import { HiOutlineMenu } from "react-icons/hi"
 
 
 import { logo } from "../assets"
@@ -31,9 +32,27 @@ const Sidebar = () => {
   
   return (
     <>
-      < div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
+      </div>
+
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine className="w-5 h-6 text-white mr-2"
+          onClick={() => setMobileMenuOpen(false)}
+          /> 
+        ) :
+          <HiOutlineMenu className="w-5 h-6 text-white mr-2"
+          onClick={() => setMobileMenuOpen(true)}
+          />
+        }
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] 
+      backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0': '-left-full'}`}>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)}/>
       </div>
     </>
   )
