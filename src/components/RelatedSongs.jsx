716283f6d41e093a8cb5dfdc88912409bb35a29d
D-coder <chuksjoshuaa@gmail.com>
2022-10-09 18:47:06 +0100

    Updated Changes to Related Songs

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import SongBar from "./SongBar";
 
-const RelatedSongs = () => {
-  return <div>RelatedSongs</div>;
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+  artistId,
+}) => {
+  return (
+    <div className="flex flex-col">
+      <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+      <div>
+        {data?.map((song, i) => (
+          <SongBar
+            key={`${song.key}-${artistId}`}
+            song={song}
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default RelatedSongs;
