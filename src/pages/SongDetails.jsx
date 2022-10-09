22f3dca8b501f8467c5dec3e38138aa10b66bb22
D-coder <chuksjoshuaa@gmail.com>
2022-10-09 16:17:47 +0100

    WIP: Working on DetailsHeader

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -16,7 +16,7 @@ const SongDetails = () => {
 
   return (
     <div className="flex flex-col">
-      {/* <DetailsHeader artistId={artistsId} songData={songData} /> */}
+      <DetailsHeader artistId="" songData={songData} />
 
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
@@ -24,12 +24,18 @@ const SongDetails = () => {
         <div className="mt-5">
           {songData?.sections[1].type === "LYRICS" ? (
             songData?.sections[1].text.map((line, i) => (
-              <p key={line.title} i={i} className="">
+              <p
+                key={line.title}
+                i={i}
+                className="text-gray-400 text-base my-1"
+              >
                 {line}
               </p>
             ))
           ) : (
-            <p >Sorry, no lyrics found!</p>
+            <p className="text-gray-400 text-base my-1">
+              Sorry, no lyrics found!
+            </p>
           )}
         </div>
       </div>
