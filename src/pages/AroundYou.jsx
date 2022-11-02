ca5ff218426e0e56107d7981c6041da645bc33f6
Chuks Joshua <chuksmbanasoj@gmail.com>
2022-11-02 15:04:58 +0100

    WIP: Added Top Artists

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -34,13 +34,16 @@ const CountryTracks = () => {
     <div className="flex flex-col">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
         Around You
-        <span className="font-bold"> ({country})</span>
+        <span className="font-bold">
+          {" "}
+          {country === "" ? null : `(${country})`}
+        </span>
       </h2>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
           <>
             <SongCard
-              key={`${song.key} - ${i}`}
+              key={`${song.key}-${i}`}
               song={song}
               isPlaying={isPlaying}
               activeSong={activeSong}
--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -21,7 +21,7 @@ const TopCharts = () => {
         {data?.map((song, i) => (
           <>
             <SongCard
-              key={`${song.key} - ${i}`}
+              key={`${song.key}-${i}`}
               song={song}
               isPlaying={isPlaying}
               activeSong={activeSong}
